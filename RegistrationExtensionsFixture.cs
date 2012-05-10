6c2256ef236c33b1aa18b7731ae16bf44f8a544c
travis.illig@gmail.com <unknown@kiln.example.com>
2012-05-10 22:02:17 +0700

    Moved core test assemblies to new location.

---
+++ RegistrationExtensionsFixture.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Autofac.Builder;
+using Autofac.Integration.Web;
+using Autofac.Core.Lifetime;
+using Autofac.Core;
+
+namespace Autofac.Tests.Integration.Web
+{
+    [TestFixture]
+    public class RegistrationExtensionsFixture
+    {
+        [Test]
+        public void HttpRequestScopedIsASynonymForMatchingRequestLifetime()
+        {
+            var cb = new ContainerBuilder();
+            var regId = cb.RegisterType<object>().InstancePerHttpRequest().RegistrationStyle.Id;
+            var c = cb.Build();
+            var reg = (IComponentRegistration)c.ComponentRegistry.Registrations
+                .Where(cr => cr.Id == regId)
+                .Single();
+
+            Assert.IsInstanceOf<MatchingScopeLifetime>(reg.Lifetime);
+            Assert.AreEqual(InstanceSharing.Shared, reg.Sharing);
+        }
+    }
+}
