9dbfac5195708c12a6a31436f63f89ebc9b0af32
Travis Illig <tillig@paraesthesia.com>
2022-07-21 14:10:28 -0700

    Remove unnecessary usings.

--- src/Autofac.Integration.Web/Properties/AssemblyInfo.cs
+++ src/Autofac.Integration.Web/Properties/AssemblyInfo.cs
@@ -1,9 +1,6 @@
 ﻿// Copyright (c) Autofac Project. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-using System;
-using System.Reflection;
-using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
